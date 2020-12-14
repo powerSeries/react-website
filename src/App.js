657530710266eebeaf35e90168087279fdab3b39
powerSeries <joseesotolongo@gmail.com>
2020-12-13 21:45:39 -0500

    deploy my react website to github pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React Website</h1>
+      <h2>Author: Jose Sotolongo</h2>
+    </div>
+  );
+}
+
+export default App;
